7184cc08273799388316ee1df1634c9857d72a4e
ova <chivomada8@yandex.ru>
2018-02-08 18:46:13 +0300

    full_burger_7

--- js/scripts.js
+++ js/scripts.js
@@ -29,13 +29,14 @@ var teamName = document.querySelector(".team__name");
 var teamPerson = document.querySelector(".team__person");
 var teamItem = document.querySelectorAll(".team__item");
 
+// Без этого блока первый элемент не открывается //
 teamName.addEventListener('click', function (e) {
     e.preventDefault();    
     teamName.classList.toggle('team__name-active');
     teamPerson.classList.toggle('team__person-active');     
 });
 
-for (var i = 0; i < teamItem.length; i++) {
+for (var i = 0; i < teamItem.length; i++) {    
     teamName[i].addEventListener('click', function () {  
     teamName.classList.toggle('team__name-active');
     teamPerson.classList.toggle('team__person-active');   
@@ -49,14 +50,8 @@ var menuName = document.querySelector(".menu__name");
 var menuText = document.querySelector(".menu__text");
 var menuItem = document.querySelectorAll(".menu__item");
 
-
-menuItem.addEventListener('click', function (e) {
-    e.preventDefault();    
-    menuText.classList.toggle('menu__text-active');      
-    menuName.classList.toggle('menu__name-active');         
-});
-
 for (var i = 0; i < menuItem.length; i++) {
+    e.preventDefault();
     menuItem[i].addEventListener('click', function () {  
     menuText.classList.toggle('menu__text-active');       
     menuName.classList.toggle('menu__name-active');        
