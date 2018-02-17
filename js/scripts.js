8d00034c04bcee2ba3d88114d5e55a0b926045cc
ova <chivomada8@yandex.ru>
2018-02-17 19:23:21 +0300

    full_burger_9

--- js/scripts.js
+++ js/scripts.js
@@ -21,44 +21,88 @@ for (var i = 0; i < dropdownItem.length; i++) {
 })}
 
 
-// Вертикальный аккордеон - Команда //
-var teamItem = document.querySelectorAll(".team__item");
 
-for (let i = 0; i < teamItem.length; i++) {
-    teamItem[i].addEventListener ('click', function (el) {
-        el.currentTarget.classList.toggle('team__item-active');
+// OPS //
+$('#fullpage').fullpage({
+    menu: '#nav_list' 
+});
 
-        for (let i = 0; i < teamItem.length; i++) {
-            if (teamItem[i] !== el.currentTarget && teamItem[i].className === "team__item team__item-active" ) {
-                itemV[i].classList.remove('team__item-active');
-            }
-        }
-    })
-};
+var navigationItem = document.querySelectorAll(".navigation__item");
+var navigationLink = document.querySelector(".navigation__link");
 
-
-// Горизонтальный аккордеон - Меню //
-
-var menuItem = document.querySelectorAll(".menu__item");
-
-for (let i = 0; i < menuItem.length; i++) {
-    menuItem[i].addEventListener ('click', function (el) {
-        el.currentTarget.classList.toggle('menu__item-active');
-
-        for (let i = 0; i < menuItem.length; i++) {
-            if (menuItem[i] !== el.currentTarget && menuItem[i].className === "menu__item menu__item-active" ) {
-                menuV[i].classList.remove('menu__item-active');
-            }
-        }
-    })
-};
-
-
-
+for (var i = 0; i < navigationLink.length; i++) {
+    navigationLink[i].addEventListener('click', function () {
+    navigationItem.classList.toggle('navigation__item_active');   
+})};
 
 
 
 // Слайдер //
+
+$('.slider__list').slick();
+
+
+
+
+
+// Вертикальный аккордеон - Команда //
+
+var teamName = document.querySelectorAll(".team__name");
+var teamItem = document.querySelectorAll(".team__item");
+
+for (let i = 0; i < teamName.length; i++) {
+    teamName[i].addEventListener('click', function (e) {
+        e.preventDefault();
+
+        for (let j = 0; j < teamItem.length; j++) {
+            if (j !== i) {
+                teamItem[j].classList.remove("team__item-active");
+            };
+        };
+
+        this.closest('.team__item').classList.toggle("team__item-active");
+    });
+};
+
+
+
+
+// Горизонтальный аккордеон - Меню //
+
+var menuName = document.querySelectorAll(".menu__name");
+var menuItem = document.querySelectorAll(".menu__item");
+
+for (let i = 0; i < menuName.length; i++) {
+    menuName[i].addEventListener('click', function (e) {
+        e.preventDefault();
+
+        for (let j = 0; j < menuItem.length; j++) {
+            if (j !== i) {
+                menuItem[j].classList.remove("menu__item-active");
+            };
+        };
+
+        this.closest('.menu__item').classList.toggle("menu__item-active");
+    });
+};
+
+
+
+
+// Отзывы //
+
+function openPopup(e) {
+    e.preventDefault();
+    $('.reviews__popup').toggleClass('reviews__popup-active');
+};
+
+$('.reviews__list .btn').on('click', openPopup);
+$('.reviews__popup-close').on('click', openPopup);
+
+
+
+
+/* Слайдер 
 
 const left = document.querySelector("#left");
 const right = document.querySelector("#right");
@@ -82,3 +126,4 @@ left.addEventListener("click", function() {
     list.style.right = currentRight + "px";
   }
 });
+*/
