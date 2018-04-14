5492d25a521cbbb4e2cae6a56663797e083d2306
ova <chivomada8@yandex.ru>
2018-04-14 06:32:46 +0300

    MT

--- server.php
+++ server.php
@@ -1,18 +1,23 @@
+
 <?php
 
-    $name = $_POST['user-name'];
-    $phone = $_POST['user-phone'];
-    $street = $_POST['user-street'];
-    $house = $_POST['user-house'];
-    $housing = $_POST['user-housing'];
-    $apartment = $_POST['user-apartment'];
-    $floor = $_POST['user-floor'];
-    $comment = $_POST['user-comment'];
-    $pay = $_POST['pay'];    
+    $object = $_POST['user-object'];
+    $length = $_POST['user-length'];
+    $width = $_POST['user-width'];
+    $height = $_POST['user-height'];
 
     $notcall = $_POST['notcall']; // 1 или null
     $notcall = isset($notcall) ? 'НЕТ' : 'ДА';
+    
+    $adress = $_POST['user-adress'];
+    $name2 = $_POST['user-name2'];
+    $phone2 = $_POST['user-phone2'];    
+    $mail2 = $_POST['user-mail2'];
+    
+    
 
+
+    
     $mail_message = '
     <html>
     <head>
@@ -21,21 +26,22 @@
     <body>
         <h2>Заказ</h2>
         <ul>
-            <li>Имя: ' . $name . '</li>
-            <li>Телефон: ' . $phone . '</li>            
-            <li>Улица: ' . $street . '</li>
-            <li>Дом: ' . $house . '</li>
-            <li>Корпус: ' . $housing . '</li>
-            <li>Квартира: ' . $apartment . '</li>
-            <li>Этаж: ' . $floor . '</li>
-            <li>Комментарий к заказу: ' . $comment . '</li>
-            <li>Оплата: ' . $pay . '</li>
+            <li>Объект: ' . $object . '</li>
+            <li>Длина: ' . $length . '</li>            
+            <li>Ширина: ' . $width . '</li>
+            <li>Высота: ' . $height . '</li>
+
             <li>Нужно ли перезванивать клиенту: ' . $notcall . '</li>
+
+            <li>Адрес: ' . $adress . '</li>
+            <li>Имя: ' . $name2 . '</li>
+            <li>Телефон: ' . $phone2 . '</li>
+            <li>email: ' . $mail2 . '</li>           
         </ul>
     </body>
     </html>';
 
-    $headers = "From: Администратор сайта <olga-slavina7@yandex.ru>\r\n".
+    $headers = "From: Расчёт сметы <olga-slavina7@yandex.ru>\r\n".
                 "MIME-Version: 1.0" . "\r\n" .
                 "Content-type: text/html; charset=UTF-8" . "\r\n";
 
